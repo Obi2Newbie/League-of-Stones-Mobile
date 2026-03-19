7cc7db6c4fd8b5abd4bb7fde572b01dc9fd85bcf
Kevin Roussety <kevinroussety1@gmail.com>
2026-03-19 11:24:06 +0100

    Nativewind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./screens/**/*.{js,jsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
